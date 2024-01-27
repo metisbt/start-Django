2b1a08ca6b69ef23489f0fe1c3daa499446cab35
Mohamad Mahdi Sabet <77541798+metisbt@users.noreply.github.com>
2024-01-27 19:37:57 +0330

    created models for blog post and website contact
    + table post
    
    + table contact

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 
 class Post(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length = 255)
     content = models.TextField()
+    # image
+    # tag
+    # category
+    # author
+    counted_view = models.IntegerField(default = 0)
+    status = models.BooleanField(default = False)
+    created_date = models.DateTimeField(auto_now_add = True)
+    updated_time = models.DateTimeField(auto_now = True)
+    published_date = models.DateTimeField(null = True)
