cb17963c85c4e14f7f0e527523a6480cc1d65c66
Mohamad Mahdi Sabet <77541798+metisbt@users.noreply.github.com>
2024-01-29 06:39:56 +0330

    added function in view of blog app
    + added function that show posts that published date Less than or equal to now.
    
    + added function that count view of single post.

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from blog.models import Post
+from django.utils import timezone
+
 
 def blog_view(request):
-    return render(request, 'blog/blog-home.html')
+    posts = Post.objects.all()
 
-def blog_single(request):
-    return render(request, 'blog/blog-single.html')
+    for post in posts:
+        if post.published_date <= timezone.now():
+            post.status = True
+        else:
+            continue
+
+    context = {'posts' : posts}
+    return render(request, 'blog/blog-home.html', context)
+
+def blog_single(request, pid):
+    post = get_object_or_404(Post, id = pid)
+    post.counted_view += 1
+    context = {'post' : post}
+    return render(request, 'blog/blog-single.html', context)
