64612a11768035fe7e237f0bb647354557d29e60
Mohamad Mahdi Sabet <77541798+metisbt@users.noreply.github.com>
2024-01-31 17:47:18 +0330

    added author filed
    added author and use foreignkey from user tabel

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-from django.utils import timezone
-from datetime import datetime, timezone
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length = 255)
@@ -8,7 +7,7 @@ class Post(models.Model):
     # image
     # tag
     # category
-    # author
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     counted_view = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
     created_date = models.DateTimeField(auto_now_add=True)
