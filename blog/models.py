eeb433251c9909807cc88da3c333d29e7465008b
Mohamad Mahdi Sabet <77541798+metisbt@users.noreply.github.com>
2024-01-31 23:59:53 +0330

    added categories
    + added category in admin panel
    
    + added to post app
    
    + added to blog_home and blog_single

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Category(models.Model):
+    name = models.CharField(max_length = 255)
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     title = models.CharField(max_length = 255)
     content = models.TextField()
     image = models.ImageField(upload_to='blog/',default='blog/default.jpg')
     # tag
-    # category
+    category = models.ManyToManyField(Category)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     counted_view = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
@@ -22,4 +28,3 @@ class Post(models.Model):
 
     def __str__ (self):
         return self.title
-    
