d90eeae3ab7eb3ee7a41b63da685c90a8617d2f3
Mohamad Mahdi Sabet <77541798+metisbt@users.noreply.github.com>
2024-02-04 17:23:48 +0330

    categories show with inclusion tgas

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,3 +1,18 @@
 from django import template
+from blog.models import Post, Category
 
 register = template.Library()
+
+@register.inclusion_tag('blog/blog-lates-posts.html')
+def latestposts(arg=4):
+    posts = Post.objects.filter(status=1).order_by('-published_date')[:arg]
+    return {'posts' : posts}
+
+@register.inclusion_tag('blog/blog-post-categories.html')
+def postcategories():
+    posts = Post.objects.filter(status=1)
+    categories = Category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name]=posts.filter(category=name).count
+    return {'categories' : cat_dict}
