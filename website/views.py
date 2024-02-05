4e5d370bd0983657d221f7a0aedea0f4f4f0985d
Mohamad Mahdi Sabet <77541798+metisbt@users.noreply.github.com>
2024-02-05 12:16:59 +0330

    changed ContactForm name to NameForm

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from website.forms import NameForm, ContactFrorm
 
 def index_view(request):
     return render(request, 'website/index.html')
@@ -8,4 +9,10 @@ def about_view(request):
     return render(request, 'website/about.html')
 
 def contact_view(request):
+    if request.method == 'POST':
+        form = NameForm(request.POST)
+        if form.is_valid():
+            pass
+
+
     return render(request, 'website/contact.html')
