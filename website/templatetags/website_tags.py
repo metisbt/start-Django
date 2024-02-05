97631698cac6c955b306cea109b81bc4c6caf29f
Mohamad Mahdi Sabet <77541798+metisbt@users.noreply.github.com>
2024-02-05 10:47:32 +0330

    Update website_tags.py
    update website_tags.py for display posts filter by published_date and status

--- website/templatetags/website_tags.py
+++ website/templatetags/website_tags.py
@@ -1,9 +1,11 @@
 from django import template
 from blog.models import Post
+from django.utils import timezone
+from datetime import datetime, timezone
 
 register = template.Library()
 
 @register.inclusion_tag('website/latest-post-home.html')
 def latest_post_home():
-    posts = Post.objects.filter(status=1).order_by('-published_date')[:6]
+    posts = Post.objects.filter(published_date__lte=datetime.now(tz=timezone.utc), status=1).order_by('-published_date')[:6]
     return {'posts' : posts}
