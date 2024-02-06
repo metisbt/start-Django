ab32abf2c864a9c9633ff0aac2bcea27c207d80e
Mohamad Mahdi Sabet <77541798+metisbt@users.noreply.github.com>
2024-02-07 01:23:01 +0330

    added sites framework to setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,8 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website.apps.WebsiteConfig',
+    'django.contrib.sites',
     'blog.apps.BlogConfig'
 ]
+
+SITE_ID = 2
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
