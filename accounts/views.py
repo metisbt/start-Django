48d45663cccfb1334d2ce15011fcd7b1e1a7bbce
Mohamad Mahdi Sabet <77541798+metisbt@users.noreply.github.com>
2024-02-09 23:50:10 +0330

    added authenticated to login_view

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
 def login_view(request):
-    return render(request, 'accounts/login.html')
+    if request.user.is_authenticated:
+        msg = f'user is authenticated as {request.user.username}'
+    else:
+        msg = 'user is not authenticated!'
+    
+    context = {'msg' : msg}
+    return render(request, 'accounts/login.html', context)
 
 # def logout_view(request):
 #     pass
