c79e5df85eae3276966bc9e188932b804dde2c24
Mohamad Mahdi Sabet <77541798+metisbt@users.noreply.github.com>
2024-02-09 21:18:03 +0330

    added comment class to blog models.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Post, Category
+from blog.models import Post, Category, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -10,8 +10,16 @@ class PostAdmin(SummernoteModelAdmin):
     list_display = ('title', 'author', 'counted_view', 'status', 'published_date', 'created_date')
     list_filter = ('status','author')
     # ordering = ('-created_date',)
-    search_fields = ('title', 'content')
+    search_fields = ['title', 'content']
     summernote_fields = ('content',)
 
+class CommentAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ('name', 'post', 'approved', 'created_date')
+    list_filter = ('post','approved')
+    search_fields = ['name', 'post']
+
+admin.site.register(Comment, CommentAdmin)
 admin.site.register(Category)
 # admin.site.register(Post, PostAdmin)
