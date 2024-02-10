46bb1cc22b7fdda86b4034271708c2de0f5d3ff0
Mohamad Mahdi Sabet <77541798+metisbt@users.noreply.github.com>
2024-02-10 15:08:12 +0330

    changed login form and write base on authenticationform

--- accounts/views.py
+++ accounts/views.py
@@ -1,20 +1,26 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+
 
 
 def login_view(request):
-    if request.method == 'POST':
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('/')
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = AuthenticationForm(request=request, data=request.POST)
+            if form.is_valid():
+                username = form.cleaned_data.get('username')
+                password = form.cleaned_data.get('password')
+                user = authenticate(request, username=username, password=password)
+                if user is not None:
+                    login(request, user)
+                    return redirect('/')
+        
+        form = AuthenticationForm()
+        context = {'form' : form}
+        return render(request, 'accounts/login.html', context)
     else:
-        msg = 'user is not authenticated!'
-    
-    context = {'msg' : msg}
-    return render(request, 'accounts/login.html', context)
+        return redirect('/')
 
 # def logout_view(request):
 #     pass
